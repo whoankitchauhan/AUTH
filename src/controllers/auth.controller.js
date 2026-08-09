14da07e3d6d883ec511fa105d0aa43b050029249
Ankit Chauhan <whoankitchauhan@gmail.com>
2026-08-09 23:57:07 +0530

    refactor: streamline getMe function for improved readability and consistency

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -47,18 +47,23 @@ export async function register(req, res) {
   });
 }
 
-
 export async function getMe(req, res) {
-    const token = req.headers.authorization?.split(" ")[1];
-    if (!token) {
-      return res.status(401).json({ message: "Unauthorized" });
-    }
+  const token = req.headers.authorization?.split(" ")[1];
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
 
-    const decoded = jwt.verify(token, config.JWT_SECRET);
+  const decoded = jwt.verify(token, config.JWT_SECRET);
 
-    console.log(decoded);
-    return res.status(200).json({ 
-      message: "User authenticated", 
-      userId: decoded.id
-     });
+  console.log(decoded);
+
+  const user = await userModel.findById(decoded.id);
+  res.status(200).json({
+    message: "User fetched successfully",
+    user: {
+      id: user._id,
+      username: user.username,
+      email: user.email,
+    },
+  });
 }
