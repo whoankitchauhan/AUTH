b72e20cf39cad8b530939ac7e24c7a9d0397b239
Ankit Chauhan <whoankitchauhan@gmail.com>
2026-08-09 23:36:16 +0530

    feat: implement token verification in getMe function

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -49,5 +49,12 @@ export async function register(req, res) {
 
 
 export async function getMe(req, res) {
-    
+    const token = req.headers.authorization?.split(" ")[1];
+    if (!token) {
+      return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    const decoded = jwt.verify(token, config.JWT_SECRET);
+
+    console.log(decoded);
 }
