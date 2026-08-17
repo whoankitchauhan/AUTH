9d5b1a5dde641f8ff642de377d0ed7b5028369ad
Ankit Chauhan <whoankitchauhan@gmail.com>
2026-08-18 00:08:35 +0530

    feat: add login and logout-all routes to authRouter

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -4,7 +4,9 @@ import * as authController from "../controllers/auth.controller.js";
 const authRouter = Router();
 
 authRouter.post("/register", authController.register);
+authRouter.post("/login", authController.login);
 authRouter.get("/get-me", authController.getMe);
 authRouter.get("/refresh-token", authController.refreshToken);
 authRouter.get("/logout",authController.logout);
+authRouter.get("/logout-all",authController.logoutAll);
 export default authRouter;
