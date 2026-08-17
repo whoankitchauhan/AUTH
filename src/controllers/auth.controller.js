27e13d38870bb374d6ec907d55e4683efcbfb2bb
Ankit Chauhan <whoankitchauhan@gmail.com>
2026-08-17 17:27:05 +0530

    feat: implement session model and enhance token handling in register function

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -2,6 +2,7 @@ import userModel from "../models/user.model.js";
 import crypto from "crypto";
 import config from "../config/config.js";
 import jwt from "jsonwebtoken";
+import sessionModel from "../models/session.model.js";
 
 export async function register(req, res) {
   const { username, email, password } = req.body;
@@ -26,16 +27,6 @@ export async function register(req, res) {
     password: hashedPassword,
   });
 
-  const accessToken = jwt.sign(
-    {
-      id: user._id,
-    },
-    config.JWT_SECRET,
-    {
-      expiresIn: "15m",
-    },
-  );
-
   const refreshToken = jwt.sign(
     {
       id: user._id,
@@ -45,6 +36,30 @@ export async function register(req, res) {
       expiresIn: "7d",
     },
   );
+
+  const refreshTokenHash = crypto
+    .createHash("sha256")
+    .update(refreshToken)
+    .digest("hex");
+
+    const session = await sessionModel.create({
+      userId: user._id,
+      refreshTokenHash,
+      ip: req.ip,
+      userAgent: req.headers["user-agent"],
+    });
+    
+  const accessToken = jwt.sign(
+    {
+      id: user._id,
+      sessionId: session._id,
+    },
+    config.JWT_SECRET,
+    {
+      expiresIn: "15m",
+    },
+  );
+
   res.cookie("refreshToken", refreshToken, {
     httpOnly: true,
     secure: true,
@@ -109,7 +124,7 @@ export async function refreshToken(req, res) {
       expiresIn: "7d",
     },
   );
-  
+
   res.cookie("refreshToken", newRefreshToken, {
     httpOnly: true,
     secure: true,
---
+++ src/models/session.model.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const sessionSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "users",
+      required: [true, "User ID is required"],
+    },
+    refreshTokenHash: {
+      type: String,
+      required: [true, "Refresh token hash is required"],
+    },
+    ip: {
+      type: String,
+      required: [true, "IP address is required"],
+    },
+    userAgent: {
+      type: String,
+      required: [true, "User agent is required"],
+    },
+    revoked: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const sessionModel = mongoose.model("sessions", sessionSchema);
+export default sessionModel;
