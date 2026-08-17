347d8289711922fbb9cf5b976a7c3a18831be7ce
Ankit Chauhan <whoankitchauhan@gmail.com>
2026-08-18 00:08:29 +0530

    feat: implement login functionality and enhance logout features

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -77,6 +77,80 @@ export async function register(req, res) {
   });
 }
 
+export async function login(req, res) {
+  const { email, password } = req.body;
+
+  const user = await userModel.findOne({ email });
+
+  if (!user) {
+    return res.status(400).json({
+      message: "Invalid Email or Password",
+    });
+  }
+
+  const hashedPassword = crypto
+    .createHash("sha256")
+    .update(password)
+    .digest("hex");
+
+  const isPasswordValid = hashedPassword === user.password;
+  if (!isPasswordValid) {
+    return res.status(400).json({
+      message: "Invalid Email or Password",
+    });
+  }
+
+  const refreshToken = jwt.sign(
+    {
+      id: user._id,
+    },
+    config.JWT_SECRET,
+    {
+      expiresIn: "7d",
+    },
+  );
+
+  const refreshTokenHash = crypto
+    .createHash("sha256")
+    .update(refreshToken)
+    .digest("hex");
+
+  const session = await sessionModel.create({
+    userId: user._id,
+    refreshTokenHash,
+    ip: req.ip,
+    userAgent: req.headers["user-agent"],
+  });
+
+  const accessToken = jwt.sign(
+    {
+      id: user._id,
+      sessionId: session._id,
+    },
+    config.JWT_SECRET,
+    {
+      expiresIn: "15m",
+    },
+  );
+
+  res.cookie("refreshToken", refreshToken, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "strict",
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+  });
+
+  res.status(201).json({
+    message: "Logged In successfully",
+    user: {
+      id: user._id,
+      username: user.username,
+      email: user.email,
+    },
+    accessToken,
+  });
+}
+
 export async function getMe(req, res) {
   const token = req.headers.authorization?.split(" ")[1];
   if (!token) {
@@ -139,6 +213,14 @@ export async function refreshToken(req, res) {
     },
   );
 
+  const newRefreshTokenHash = crypto
+    .createHash("sha256")
+    .update(newRefreshToken)
+    .digest("hex");
+
+  session.refreshTokenHash = newRefreshTokenHash;
+  await session.save();
+
   res.cookie("refreshToken", newRefreshToken, {
     httpOnly: true,
     secure: true,
@@ -175,4 +257,36 @@ export async function logout(req, res) {
 
   session.revoked = true;
   await session.save();
+
+  res.clearCookie("refreshToken");
+
+  res.status(200).json({
+    message: "Logged out successfully",
+  });
+}
+
+export async function logoutAll(req, res) {
+  const refreshToken = req.cookies.refreshToken;
+
+  if (!refreshToken) {
+    return res.status(401).json({ message: "Refresh Token not found" });
+  }
+
+  const decoded = jwt.verify(refreshToken, config.JWT_SECRET);
+
+  await sessionModel.updateMany(
+    {
+      user: decoded.id,
+      revoked: false,
+    },
+    {
+      revoked: true,
+    },
+  );
+
+  res.clearCookie("refreshToken");
+
+  res.status(200).json({
+    message: "Logged Out from all Devices",
+  });
 }
