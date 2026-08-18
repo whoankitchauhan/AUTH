2c397622c75fab0d160632bc8ed51da837e207d6
Ankit Chauhan <whoankitchauhan@gmail.com>
2026-08-19 00:12:35 +0530

    feat: add missing Google environment variable checks and include them in config

--- src/config/config.js
+++ src/config/config.js
@@ -9,9 +9,29 @@ if (!process.env.JWT_SECRET) {
   throw new Error("JWT_SECRET is not defined in the environment variables");
 }
 
+if (!process.env.GOOGLE_CLIENT_ID) {
+  throw new Error("GOOGLE_CLIENT_ID is not defined in the environment variables");
+}
+
+if (!process.env.GOOGLE_CLIENT_SECRET) {
+  throw new Error("GOOGLE_CLIENT_SECRET is not defined in the environment variables");
+}
+
+if (!process.env.GOOGLE_REFRESH_TOKEN) {
+  throw new Error("GOOGLE_REFRESH_TOKEN is not defined in the environment variables");
+}
+
+if (!process.env.GOOGLE_USER) {
+  throw new Error("GOOGLE_USER is not defined in the environment variables");
+}
+
 const config = {
     MONGO_URI: process.env.MONGO_URI,
     JWT_SECRET: process.env.JWT_SECRET,
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+    GOOGLE_REFRESH_TOKEN: process.env.GOOGLE_REFRESH_TOKEN,
+    GOOGLE_USER: process.env.GOOGLE_USER,
 }
 
 
