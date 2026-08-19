ff513895cc53a693ff00ede15e57ab4dc0177d62
Ankit Chauhan <whoankitchauhan@gmail.com>
2026-08-19 23:52:40 +0530

    feat: add email verification route to authentication API

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -9,4 +9,5 @@ authRouter.get("/get-me", authController.getMe);
 authRouter.get("/refresh-token", authController.refreshToken);
 authRouter.get("/logout",authController.logout);
 authRouter.get("/logout-all",authController.logoutAll);
+authRouter.post("/verify-email", authController.verifyEmail);
 export default authRouter;
