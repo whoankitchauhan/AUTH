aa30c416f33857a7310383621669afa3799937a6
Ankit Chauhan <whoankitchauhan@gmail.com>
2026-08-20 00:41:38 +0530

    fix: ensure OTP is returned as a string in generateOtp function

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,5 +1,5 @@
 export function generateOtp() {
-    return Math.floor(100000 + Math.random() * 900000);
+  return Math.floor(100000 + Math.random() * 900000).toString();
 }
 
 export function OtpHTML(otp) {
